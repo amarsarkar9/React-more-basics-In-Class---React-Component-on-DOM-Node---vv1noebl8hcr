f59e7974ce6929d1e1bb50eaa123edc32dd6e4c1
amar sarkar <amarsarkar9@gmail.com>
2022-02-03 23:15:25 +0530

    int

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import { defaultMaxListeners } from 'jsdom/lib/jsdom/virtual-console';
+
 import React from 'react';
 
 const App=()=>{
     return(
-        <>
+        <div>
         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        </>
+        </div>
     );
 };
 export default App;
